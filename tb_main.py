6c9214f63d5e0ebb756ad5f19499b6e6488f5731
MrTLane <tomlmlane@gmail.com>
2018-10-09 15:12:04 +0100

    commit for run

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_full_dev.txt'
+    file_out_name = 'out_half_dev.txt'
 
     create_out_file(file_out_name)
 
@@ -82,7 +82,7 @@ def __main__():
 
     # Define the number of cells either side of whatever interface we are using
     cell_num_L = 500        # 500
-    cell_num_R = None          # If None this is set to equal cell_num_L
+    cell_num_R = 0          # If None this is set to equal cell_num_L
 
     stripe_len = 1000       # 1000 / 1500 (sum of cell_num usually)
 
