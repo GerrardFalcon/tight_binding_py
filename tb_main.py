dc747712c6a6fda41953086e27f00253518741fb
MrTLane <tomlmlane@gmail.com>
2018-10-09 15:46:37 +0100

    commit for run

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_half_dev.txt'
+    file_out_name = 'out_half_dev_ac.txt'
 
     create_out_file(file_out_name)
 
@@ -121,7 +121,7 @@ def __main__():
         'is_wrap_finite':   True,
 
         # orientation of the cells along the x-direction perp. to transport
-        'orientation'   :   'zz',          
+        'orientation'   :   'ac',          
         'scaling'       :   SF,             # Value by which to scale the system
         }
 
