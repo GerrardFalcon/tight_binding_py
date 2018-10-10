22e4a34612d7cc0faf5166332a7524bdef8c4ccc
MrTLane <tomlmlane@gmail.com>
2018-10-10 15:28:27 +0100

    update tb_main for run

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_full_dev_zz.txt'
+    file_out_name = 'out_full_dev_ac.txt'
 
     create_out_file(file_out_name)
 
@@ -122,7 +122,7 @@ def __main__():
         'is_wrap_finite':   True,
 
         # orientation of the cells along the x-direction perp. to transport
-        'orientation'   :   'zz',          
+        'orientation'   :   'ac',          
         'scaling'       :   SF,             # Value by which to scale the system
         }
 
