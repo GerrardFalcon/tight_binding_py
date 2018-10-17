b01d6044f1ff64ccc0661c17d212430aa8f10c81
MrTLane <tomlmlane@gmail.com>
2018-10-17 13:29:07 +0100

    upload of tb_main for run

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_full_dev_ac.txt'
+    file_out_name = 'out_zz_lead_00.txt'
 
     create_out_file(file_out_name)
 
