e9d6a92e8eb57e58e18ec3e1c97a6cded90639e6
MrTLane <tomlmlane@gmail.com>
2018-10-17 13:36:56 +0100

    upload of tb_main for run

--- sys_funcs_infinite.py
+++ sys_funcs_infinite.py
@@ -433,7 +433,7 @@ def sys_infinite(pot, pot_kwargs, dev_kwargs, prog_kwargs, is_plot = True,
     params_to_txt(file_name, param_dict)
 
     lim = 0.075
-    en_list = np.linspace(0.035, 0.055, 400)#np.linspace(-lim, lim, 1200)#
+    en_list = np.linspace(0., 0.06, 400)#np.linspace(-lim, lim, 1200)#
 
     k_list = [0]#np.linspace(-np.pi, np.pi, 400)
 
