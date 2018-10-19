f36a53299a994f9449b3e42f4579d83efcc142bc
MrTLane <tomlmlane@gmail.com>
2018-10-19 16:07:46 +0100

    changed output file name in main

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_ac_lead_00.txt'
+    file_out_name = 'out_ac_lead_001.txt'
 
     create_out_file(file_out_name)
 
