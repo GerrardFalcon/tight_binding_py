8ef06c1e0d84e0861c53ddc7d8b5d690b328d283
MrTLane <tomlmlane@gmail.com>
2018-10-22 01:30:08 +0100

    commit for run

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_zz_lead_20.txt'
+    file_out_name = 'out_ac_shallow.txt'
 
     create_out_file(file_out_name)
 
@@ -52,7 +52,7 @@ def __main__():
     is_scale_CN = True
 
     scaling_prnt(SF, is_scale_CN)
-
+    """
     # Dictionary of paramters used to define the potential
     pot_kwargs = {
         'gap_val'           :   0.150,  # 100meV delta0
@@ -72,6 +72,27 @@ def __main__():
         'lead_offset'       :   -0.20,   # -0.1
 
         'channel_length'    :   1000,   # 1000A
+        'channel_relax'     :   100     # 100A
+        }
+        """
+
+    pot_kwargs = {
+        'gap_val'           :   0.03,  # 100meV delta0
+        'offset'            :   0,      # 0eV
+
+        'well_depth'        :   -0.0001,  # -20meV U0
+        'gap_relax'         :   0.3,    # dimensionless beta
+        'channel_width'     :   500,    # 850A / 500A
+
+        # Select if the well depth is modulated along the channel
+        'is_const_channel'  :   False,
+        # If is_const_channel is True, we can also supply a y-value for which to
+        # take a cut of the potential
+        'cut_at'            :   0,  # -(1200, 1060, 930, 800, 0) w/ d faults
+
+        'gap_min'           :   0.01,   # -40meV U0
+        'lead_offset'       :   0.0,   # -0.1
+        'channel_length'    :   1000,   # 2000A
         'channel_relax'     :   100     # 100A
         }
 
@@ -124,7 +145,7 @@ def __main__():
         'is_wrap_finite':   True,
 
         # orientation of the cells along the x-direction perp. to transport
-        'orientation'   :   'zz',          
+        'orientation'   :   'ac',          
         'scaling'       :   SF,             # Value by which to scale the system
         }
 
