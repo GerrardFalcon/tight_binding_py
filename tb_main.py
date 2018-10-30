e15b17f1ddf38dac07821e729ce3d950c8234a0a
MrTLane <tomlmlane@gmail.com>
2018-10-30 15:52:09 +0000

    commit of tb_main for run

--- tb_main.py
+++ tb_main.py
@@ -37,7 +37,7 @@ def __main__():
 
     # Use the tb_utility module to print the current date to our output file
 
-    file_out_name = 'out_ac_TRANS_50.txt'
+    file_out_name = 'out_zz_TRANS_50.txt'
 
     create_out_file(file_out_name)
 
@@ -112,7 +112,7 @@ def __main__():
 
     # 500 / 750 for finite bands
 
-    cell_num_L = 170        # 300 / 160 SCALES WITH POTENTIAL DIMENSIONS
+    cell_num_L = 300        # 300 / 160 SCALES WITH POTENTIAL DIMENSIONS
     cell_num_R = None       # If None this is set to equal cell_num_L
 
     stripe_len = 800       # 800 / 1400
@@ -165,7 +165,7 @@ def __main__():
         'is_wrap_finite':   True,
 
         # orientation of the cells along the x-direction perp. to transport
-        'orientation'   :   'ac',          
+        'orientation'   :   'zz',          
         'scaling'       :   SF,             # Value by which to scale the system
         }
 
